025d86787e05183d1d08dd2b5dc69f6cfb1a8c7b
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-10-17 23:25:18 +0200

    Don't reenable interrupts if they were disabled
    A critical section should not reenable the interrupts if they were
    disabled on interrupt in the first place. It should remember that it
    didn't disable them, so someone else must have already disabled them and
    is probably already relying on this behavior.

--- src/atomic.cpp
+++ src/atomic.cpp
@@ -24,18 +24,94 @@
  */
 
 #include <atomic>
+#include <cstdint>
+#include <type_traits>
+
+// Type traits that check if an action returns void
+template <class Action, class... Args>
+using returns_void = std::is_void<std::result_of_t<Action(Args...)>>;
+
+template <class Action, class... Args>
+inline constexpr bool returns_void_v = returns_void<Action, Args...>::value;
+
+/*
+ * @brief Gets the state of the processors interrupt mask. This is 1 if
+ * interrupts are masked. 0 otherwise.
+ */
+inline auto get_interrupt_mask() -> bool {
+  std::uint32_t primask;
+  asm volatile("mrs %0, primask" : "=r"(primask) :);
+  return primask != 0;
+}
+
+/**
+ * @brief Runs some code within a critical section. Ensures that the interrupt
+ * state is restored if it needed to disable interrupts.
+ */
+template <class Action, std::enable_if_t<std::is_invocable_v<Action> &&
+                                             !returns_void_v<Action>,
+                                         bool> = false>
+inline auto critical_section(Action action) {
+  const auto previously_enabled = get_interrupt_mask() == 0;
+  // Disable interrupts only if they were actually enabled. Otherwise there is
+  // no harm done, as they are already disabled
+  if (previously_enabled) {
+    asm volatile("cpsid i");
+  }
+
+  // We execute the action in the critical section and capture the return value
+  const auto retval = action();
+
+  // We reenable interrupts if we disabled them, otherwise someone else must
+  // already be relying on them being disabled, so it is not safe to reenable
+  // them at this point. no harm done, as they are already disabled
+  if (previously_enabled) {
+    asm volatile("cpsie i");
+  }
+  return retval;
+}
+
+/**
+ * @brief Runs some code within a critical section. Ensures that the interrupt
+ * state is restored if it needed to disable interrupts.
+ *
+ * This verision of
+ */
+template <class Action, std::enable_if_t<std::is_invocable_v<Action> &&
+                                             returns_void_v<Action>,
+                                         bool> = false>
+inline auto critical_section(Action action) {
+  const auto previously_enabled = get_interrupt_mask() == 0;
+  // Disable interrupts only if they were actually enabled. Otherwise there is
+  // no harm done, as they are already disabled
+  if (previously_enabled) {
+    asm volatile("cpsid i");
+  }
+
+  // We execute the action in the critical section
+  action();
+
+  // We reenable interrupts if we disabled them, otherwise someone else must
+  // already be relying on them being disabled, so it is not safe to reenable
+  // them at this point. no harm done, as they are already disabled
+  if (previously_enabled) {
+    asm volatile("cpsie i");
+  }
+}
+
+inline void memory_barrier() { asm volatile("dmb"); }
 
 template <class T>
 inline void atomic_store(volatile void* ptr, T value, std::memory_order order) {
   if (order != std::memory_order_relaxed) {
-    asm volatile("dmb");
+    memory_barrier();
   }
   *reinterpret_cast<volatile T*>(ptr) = value;
   switch (order) {
     case std::memory_order_seq_cst:
     case std::memory_order_acq_rel:
     case std::memory_order_acquire:
-      asm volatile("dmb");
+      memory_barrier();
       break;
     default:
       break;
@@ -44,9 +120,9 @@ inline void atomic_store(volatile void* ptr, T value, std::memory_order order) {
 
 extern "C" void __atomic_store_8(volatile void* ptr, uint64_t value,
                                  int order) {
-  asm volatile("cpsid i");
-  atomic_store(ptr, value, static_cast<std::memory_order>(order));
-  asm volatile("cpsie i");
+  critical_section([&]() {
+    atomic_store(ptr, value, static_cast<std::memory_order>(order));
+  });
 }
 
 extern "C" void __atomic_store_4(volatile void* ptr, unsigned int value,
@@ -69,23 +145,22 @@ inline T atomic_load(const volatile void* ptr, std::memory_order order) {
     case std::memory_order_seq_cst:
     case std::memory_order_acq_rel:
     case std::memory_order_release:
-      asm volatile("dmb");
+      memory_barrier();
       break;
     default:
       break;
   }
   T value = *reinterpret_cast<const volatile T*>(ptr);
   if (order != std::memory_order_relaxed) {
-    asm volatile("dmb");
+    memory_barrier();
   }
   return value;
 }
 
 extern "C" uint64_t __atomic_load_8(const volatile void* ptr, int order) {
-  asm volatile("cpsid i");
-  const auto value =
-      atomic_load<uint64_t>(ptr, static_cast<std::memory_order>(order));
-  asm volatile("cpsie i");
+  const auto value = critical_section([&]() {
+    return atomic_load<uint64_t>(ptr, static_cast<std::memory_order>(order));
+  });
   return value;
 }
 
@@ -103,18 +178,18 @@ extern "C" uint8_t __atomic_load_1(const volatile void* ptr, int order) {
 
 template <class T>
 T atomic_exchange(volatile void* ptr, T value, std::memory_order order) {
-  asm volatile("cpsid i");
-  if (order != std::memory_order_relaxed) {
-    asm volatile("dmb");
-  }
-  auto& atomic = *reinterpret_cast<volatile T*>(ptr);
-  const auto prev_val = atomic;
-  atomic = value;
-  if (order != std::memory_order_relaxed) {
-    asm volatile("dmb");
-  }
-  asm volatile("cpsie i");
-  return prev_val;
+  return critical_section([&]() {
+    if (order != std::memory_order_relaxed) {
+      memory_barrier();
+    }
+    auto& atomic = *reinterpret_cast<volatile T*>(ptr);
+    const auto prev_val = atomic;
+    atomic = value;
+    if (order != std::memory_order_relaxed) {
+      memory_barrier();
+    }
+    return prev_val;
+  });
 }
 
 extern "C" uint64_t __atomic_exchange_8(volatile void* ptr, uint64_t value,
@@ -136,4 +211,3 @@ extern "C" uint8_t __atomic_exchange_1(volatile void* ptr, uint8_t value,
                                        int order) {
   return atomic_exchange(ptr, value, static_cast<std::memory_order>(order));
 }
-
